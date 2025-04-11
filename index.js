873e4b1265a0843f6ef8a5ff3ee564fb40f8940f
bang9 <gusrn1423@naver.com>
2025-04-11 19:07:42 +0900

    feat: upgrade to lotto-archive-action

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const core = require('@actions/core');
+const { getLastLottoRound, LogLevel, LottoService } = require('@rich-automation/lotto');
+
+(async () => {
+  let lottoService = null;
+
+  try {
+    const count = parseInt(core.getInput('count', { required: false })) || 5;
+    const id = core.getInput('id', { required: true });
+    const pwd = core.getInput('pwd', { required: true });
+
+    if (count < 1 || count > 5) {
+      throw new Error("The 'count' input must be a number between 1 and 5.");
+    }
+
+    const round = getLastLottoRound();
+    core.info(`Current lotto round: ${round}`);
+
+    lottoService = new LottoService({
+      headless: true,
+      controller: 'playwright',
+      logLevel: LogLevel.DEBUG,
+    });
+    core.info('Signing in to the lotto service...');
+    await lottoService.signIn(id, pwd);
+
+    core.info(`Purchasing ${count} lotto ticket(s) for round ${round}...`);
+    const numbersArray = await lottoService.purchase(count);
+
+    core.info('Lotto tickets purchased successfully.');
+    core.setOutput('numbersArray', JSON.stringify(numbersArray));
+    core.setOutput('round', round);
+  } catch (error) {
+    core.setFailed(`Lotto purchase failed: ${error.message}`);
+  } finally {
+    if (lottoService) {
+      await lottoService.destroy();
+    }
+  }
+})();
